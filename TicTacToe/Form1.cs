f0e9b0d6f08bc78cf2a166f8283e8374a7c79f95
tOspechceva <t.ospechceva@mail.ru>
2025-05-21 00:42:31 +0300

    Добавьте файлы проекта.

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private Game game = new Game(); // Игровая логика
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Назначение координат каждой кнопке через Tag
+            button1.Tag = (0, 0);
+            button2.Tag = (0, 1);
+            button3.Tag = (0, 2);
+            button4.Tag = (1, 0);
+            button5.Tag = (1, 1);
+            button6.Tag = (1, 2);
+            button7.Tag = (2, 0);
+            button8.Tag = (2, 1);
+            button9.Tag = (2, 2);
+
+            // Подписка на событие победы
+            game.OnWin += HandleWin;
+        }
+        
+        // Кнопка: Установить игрока X
+        private void button11_Click(object sender, EventArgs e)
+        {
+            game.CurrentPlayer = Game.Player.X;
+            textBox1.Text = "Ходит X";
+        }
+
+        // Кнопка: Установить игрока O
+        private void button12_Click(object sender, EventArgs e)
+        {
+            game.CurrentPlayer = Game.Player.O;
+            textBox1.Text = "Ходит O";
+        }
+
+        // Кнопка: Очистить поле
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Очистить кнопки
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button btn && btn.Tag is ValueTuple<int, int>)
+                {
+                    btn.Text = "";
+                    btn.ForeColor = Color.Black;
+                    btn.Enabled = true;
+                }
+            }
+
+            // Очистить игровое поле и текст
+            game.Reset();
+            textBox1.Text = "Поле очищено!";
+        }
+
+        // Обработка хода игрока
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            var (row, col) = ((int, int))btn.Tag;
+
+            if (btn.Text != "" || game.CurrentPlayer == Game.Player.None)
+                return;
+
+            if (!game.MakeMove(row, col))
+                return;
+
+            btn.Text = game.CurrentPlayer.ToString();
+
+            // Проверка победы
+            var winLine = game.CheckWinnerAndRaise();
+            if (winLine != null)
+                return;
+
+            // Проверка ничьей
+            if (game.IsDraw())
+            {
+                textBox1.Text = "Ничья!";
+                return;
+            }
+
+            // Смена игрока
+            game.SwitchPlayer();
+            textBox1.Text = $"Ходит {game.CurrentPlayer}";
+        }
+
+        // Обработчик события победы
+        private void HandleWin(Game.Player winner, List<(int, int)> winLine)
+        {
+            textBox1.Text = $"Победа: {winner}";
+
+            foreach (var (r, c) in winLine)
+            {
+                var btn = GetButtonAt(r, c);
+                if (btn != null)
+                    btn.ForeColor = Color.Red;
+            }
+        }
+
+        // Получить кнопку по координатам
+        private Button GetButtonAt(int row, int col)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button btn && btn.Tag is ValueTuple<int, int> tag)
+                {
+                    if (tag.Item1 == row && tag.Item2 == col)
+                        return btn;
+                }
+            }
+            return null;
+        }
+
+        
+    }
+}
---
+++ TicTacToe/Game.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        // Игроки
+        public enum Player { None, X, O }
+
+        // Текущий игрок
+        public Player CurrentPlayer { get; set; } = Player.None;
+
+        // Игровое поле 3x3 (значения: "", "X", "O")
+        public List<List<string>> Board { get; private set; } = new List<List<string>>
+        {
+            new List<string> { "", "", "" },
+            new List<string> { "", "", "" },
+            new List<string> { "", "", "" }
+        };
+
+        // Событие, вызываемое при победе игрока
+        public event Action<Player, List<(int, int)>> OnWin;
+
+        // Выполнить ход
+        public bool MakeMove(int row, int col)
+        {
+            if (Board[row][col] != "")
+                return false;
+
+            Board[row][col] = CurrentPlayer.ToString();
+            return true;
+        }
+
+        // Сменить игрока
+        public void SwitchPlayer()
+        {
+            if (CurrentPlayer == Player.X)
+                CurrentPlayer = Player.O;
+            else if (CurrentPlayer == Player.O)
+                CurrentPlayer = Player.X;
+        }
+
+        // Проверка победителя и возврат выигрышной линии (или null)
+        public List<(int, int)> CheckWinner()
+        {
+            // Проверка строк
+            for (int i = 0; i < 3; i++)
+            {
+                if (Board[i][0] != "" &&
+                    Board[i][0] == Board[i][1] &&
+                    Board[i][1] == Board[i][2])
+                {
+                    return new List<(int, int)> { (i, 0), (i, 1), (i, 2) };
+                }
+            }
+
+            // Проверка столбцов
+            for (int j = 0; j < 3; j++)
+            {
+                if (Board[0][j] != "" &&
+                    Board[0][j] == Board[1][j] &&
+                    Board[1][j] == Board[2][j])
+                {
+                    return new List<(int, int)> { (0, j), (1, j), (2, j) };
+                }
+            }
+
+            // Диагонали
+            if (Board[0][0] != "" &&
+                Board[0][0] == Board[1][1] &&
+                Board[1][1] == Board[2][2])
+            {
+                return new List<(int, int)> { (0, 0), (1, 1), (2, 2) };
+            }
+
+            if (Board[0][2] != "" &&
+                Board[0][2] == Board[1][1] &&
+                Board[1][1] == Board[2][0])
+            {
+                return new List<(int, int)> { (0, 2), (1, 1), (2, 0) };
+            }
+
+            return null;
+        }
+
+        // Проверка победы + вызов события
+        public List<(int, int)> CheckWinnerAndRaise()
+        {
+            var winLine = CheckWinner();
+            if (winLine != null)
+                OnWin?.Invoke(CurrentPlayer, winLine);
+            return winLine;
+        }
+
+        // Проверка ничьей
+        public bool IsDraw()
+        {
+            foreach (var row in Board)
+                foreach (var cell in row)
+                    if (cell == "")
+                        return false;
+
+            return CheckWinner() == null;
+        }
+
+        // Сброс игры
+        public void Reset()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    Board[i][j] = "";
+
+            CurrentPlayer = Player.None;
+        }
+    }
+}
